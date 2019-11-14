850315790d4a824e60e062a1bed1fb02d1b42e25
Andrii Kurdiumov <kant2002@gmail.com>
2019-11-15 00:47:07 +0200

    Add test project to illustrate problem

---
+++ BenchDotNetUserSecrets/FakeService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BenchDotNetUserSecrets
+{
+    class FakeService
+    {
+        private readonly IConfiguration configuration;
+        private readonly bool useStatic;
+
+        public FakeService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            this.useStatic = this.configuration["UseStatic"] == "true";
+        }
+
+        public int GetGoogleComLength()
+        {
+            if (this.useStatic)
+            {
+                return 5;
+            }
+
+            return new Random().Next(100);
+        }
+
+        public int GetGoogleComLength2()
+        {
+            return 5;
+        }
+    }
+}
---
+++ BenchDotNetUserSecrets/SimpleBenchmark.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchDotNetUserSecrets
+{
+    [SimpleJob(launchCount: 3, warmupCount: 10, targetCount: 10)]
+    public class SimpleBenchmark
+    {
+        private FakeService service;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            var host = Host.CreateDefaultBuilder().Build();
+            var configuration = host.Services.GetService<IConfiguration>();
+
+            this.service = new FakeService(configuration);
+        }
+
+        [Benchmark]
+        public int PlaceSimplestBet()
+        {
+            return this.service.GetGoogleComLength();
+        }
+
+        [Benchmark]
+        public int PlaceSimplestBet2()
+        {
+            return this.service.GetGoogleComLength2();
+        }
+    }
+}
